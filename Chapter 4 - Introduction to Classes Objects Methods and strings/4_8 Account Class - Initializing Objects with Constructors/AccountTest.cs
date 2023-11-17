d104a5df6614833b2b935a968a2172427b97d761
Efe Can Kara <efecan_kara@proton.me>
2023-11-17 13:19:28 +0300

    Write the code of various Chapter 4 examples

--- Chapter 4 - Introduction to Classes Objects Methods and strings/4_8 Account Class - Initializing Objects with Constructors/AccountTest.cs
+++ Chapter 4 - Introduction to Classes Objects Methods and strings/4_8 Account Class - Initializing Objects with Constructors/AccountTest.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class AccountTest {
+    static void Main() {
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
+
+        Console.WriteLine($"account1 name is: {account1.Name}");
+        Console.WriteLine($"account2 name is: {account2.Name}");
+}
+}
