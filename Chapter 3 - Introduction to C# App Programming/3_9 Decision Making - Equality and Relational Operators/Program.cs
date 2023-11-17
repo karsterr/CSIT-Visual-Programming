9621c98d273f87193ec59bd1693f5e368d09e1a2
Efe Can Kara <efecan_kara@proton.me>
2023-11-17 13:42:11 +0300

    Write the code of 3.9

--- Chapter 3 - Introduction to C# App Programming/3_9 Decision Making - Equality and Relational Operators/Program.cs
+++ Chapter 3 - Introduction to C# App Programming/3_9 Decision Making - Equality and Relational Operators/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        Console.Write("Enter first integer: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second integer: ");
+        int number2 = int.Parse(Console.ReadLine());
+        if (number1 == number2){
+            Console.WriteLine($"{number1} == {number2}");
+        }
+        
+        if (number1 != number2 ){
+            Console.WriteLine($"{number1} != {number2}");
+        }
+        
+        if (number1 < number2){
+            Console.WriteLine($"{number1} < {number2}");
+        }
+        
+        if (number1 > number2){
+            Console.WriteLine($"{number1} > {number2}");
+        }
+        
+        if (number1 <= number2 ){
+            Console.WriteLine($"{number1} <= {number2}");
+        }
+        
+        if (number1 >= number2 ) {
+            Console.WriteLine($"{number1} >= {number2}");
+        }
+    }
+} 
