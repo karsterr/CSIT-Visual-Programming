bf87fd331c2e3652f5532604d77e44aafb9fda6e
Efe Can Kara <efecan_kara@proton.me>
2023-11-17 13:37:13 +0300

    Write the code of 3.6

--- Chapter 3 - Introduction to C# App Programming/3_6 Another C# App - Adding Integers/Program.cs
+++ Chapter 3 - Introduction to C# App Programming/3_6 Another C# App - Adding Integers/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program {
+
+    static void Main() {
+        int number1; 
+        int number2;
+        int sum; 
+        Console.Write("Enter first integer: "); 
+
+        number1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second integer: ");
+
+        number2 = int.Parse(Console.ReadLine());
+    sum = number1 + number2; 
+        Console.WriteLine($"Sum is {sum}"); 
+    } 
+} 
+
